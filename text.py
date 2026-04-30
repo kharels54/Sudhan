104534ef50ee35af79be95940f340a93cdd7c984
kharels54 <you@example.com>
2026-05-01 09:53:46 +1200

    commit

--- text.py
+++ text.py
@@ -1,1 +1,10 @@
-print('hello')
+print(".....Let's satart the quiz....")
+#crate a list of quiz
+quiz_data = input{'When was the New zeland win their frist WTC'
+ "option": ['a) june 22,2021, b) March 23,2022, 'c)' june23,2021 ],
+ "answer" : 'a) june 22,2021
+ }
+for i in range:
+ print(quiz_data)
+if quiz_data : 'answer'
+print( 'ringht')
