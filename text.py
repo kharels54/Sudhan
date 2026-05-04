b1d608d7357603733ff139e7119b7de307b46f26
kharels54 <you@example.com>
2026-05-05 10:59:32 +1200

    text.2

--- text.py
+++ text.py
@@ -1,17 +1,31 @@
+from re import A
+
+
 print(".....Let's satart the quiz....")
 #crate a list of quiz
-quiz_data = {
-    'When was the New zeland win their frist WTC'
-    "option": ['a) june 22,2021, b) March 23,2022, 'c)' june23,2021 ],
-    "answer" : 'a) june 22,2021
-}
+quiz_data =[
 {
-    "question": "Who's new zeland player have the most wicket in t 20"
-    "option" : [ a Trent boult , b Virat kholi, c Tim Southee ], # type: ignore
-    "answer" : c Tim Southee 
-    }  
+"question":'When was the New zeland win their frist WTC'
+"option" : ["a" june 22,2021, "b" March 23,2022, "c" june23,2021 ],
+"answer" : a
+} ,
+{
+"question": "Who's new zeland player have the most wicket in t 20"
+"option" : [ "a" Trent boult , "b" Virat kholi, "c" Tim Southee ] , # type: ignore
+"answer" : "c"
+} ,
+{
+"question": "What is the lowest score of New zeland in T20 ? "
+"option" : [ "a" 100 , "b" 49, "c" 60 ],
+"answer" : "c" 
+} ,
+{
+"Who is the most wining caption of new zeland cricket team"
+"option":["a" kane Williamson, "b" Rohit poudel, "c" Stephen Fleming] ,
+"answer" : "c"
+}  ]
         
-for items in {items['question']} 
- print(f'question')
-for items in (items{'option'})
- print('option')
+for items in quiz_data :
+ print(items['question']) 
+for opt in items:
+ print(items['option'])
