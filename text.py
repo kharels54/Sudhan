ded73a87f21b2b9f14fddb687ce70bd311ef6090
kharels54 <you@example.com>
2026-05-04 13:32:10 +1200

    changes made

--- text.py
+++ text.py
@@ -1,10 +1,17 @@
 print(".....Let's satart the quiz....")
 #crate a list of quiz
-quiz_data = input{'When was the New zeland win their frist WTC'
- "option": ['a) june 22,2021, b) March 23,2022, 'c)' june23,2021 ],
- "answer" : 'a) june 22,2021
- }
-for i in range:
- print(quiz_data)
-if quiz_data : 'answer'
-print( 'ringht')
+quiz_data = {
+    'When was the New zeland win their frist WTC'
+    "option": ['a) june 22,2021, b) March 23,2022, 'c)' june23,2021 ],
+    "answer" : 'a) june 22,2021
+}
+{
+    "question": "Who's new zeland player have the most wicket in t 20"
+    "option" : [ a Trent boult , b Virat kholi, c Tim Southee ], # type: ignore
+    "answer" : c Tim Southee 
+    }  
+        
+for items in {items['question']} 
+ print(f'question')
+for items in (items{'option'})
+ print('option')
