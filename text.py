98a3523bd93fee46e1438a6036ba890f11c34d0d
kharels54 <you@example.com>
2026-05-08 10:00:27 +1200

    new one1.0

--- text.py
+++ text.py
@@ -3,40 +3,37 @@ from re import A
 
 print(".....Let's satart the quiz....")
 #crate a list of quiz
-quiz_data = [ 
-{
-"question": "When was the New zeland win their frist WTC"
-"option" : ["A) june 22,2021", "b) March 23,2022", "C) june23,2021" ],
-"answer" : "A"
-},
-{
-"question": "Who's new zeland player have the most wicket in t 20"
-"option" : [ "A) Trent boult" , "B) Virat kholi", "C Tim Southee" ],
-"answer" : "C"
-} ,
-{
-"question": "What is the lowest score of New zeland in T20 ? "
-"option" : [ "A) 100" , "B) 49", "C 60" ],
-"answer" : "C" 
-} ,
-{
-"question": "Who is the most wining caption of new zeland cricket team"
-"option": ["A) kane Williamson", "B Rohit poudel", "C) Stephen Fleming"] ,
-"answer" : "C"
- }, ]
+quiz_question = {"When was the New zeland win their frist WTC ? " , 
+"Who's new zeland player have the most wicket in t 20 ? " , 
+"What is the lowest score of New zeland in T20 ? " , 
+"Who is the most wining caption of new zeland cricket team ?"}
+
+
+quiz_option = { "A) june 22,2021", "b) March 23,2022", "C) june23,2021", 
+"A) Trent boult" , "B) Virat kholi", "C) Tim Southee",
+ "A) 50" , "B) 40" , "C) 60" ,
+ "A) kane Williamson", "B) Rohit poudel", "C) Stephen Fleming"}
+
+quiz_answer = { "A" , "C" , "C" , "C"}
+
+
+
 score = 0
-total = quiz_data(len)
-for items in quiz_data:
- print(items['question'])
-for opt in items(['options']):
+total = quiz_question(len)
+for items in quiz_question:
+ print(items['quiz_question'])
+for quiz_opt in items(['options']):
  print(items['opt'])
 score=+1
 
-if "question" == "answer":
- print('right')
+if "quiz_question" == "quiz_answer":
+  print('right')
 else:
-  print('wrong')
+  print(f"wrong {'answer'} was the answer" )
 
 percent = (total/score)*100
 
+
 print(f"you got a {'percent'}")
+
+
