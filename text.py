dfea0aab2bd4767d0defb4e347244f9f9ffac032
kharels54 <you@example.com>
2026-05-12 14:09:00 +1200

    do it in lunch time 1.0

--- text.py
+++ text.py
@@ -1,39 +1,30 @@
 from re import A
 
 
+
 print(".....Let's satart the quiz....")
 #crate a list of quiz
-quiz_question = {"When was the New zeland win their frist WTC ? " , 
+quiz_questions = {
+"When was the New zeland win their frist WTC ? " , 
 "Who's new zeland player have the most wicket in t 20 ? " , 
 "What is the lowest score of New zeland in T20 ? " , 
 "Who is the most wining caption of new zeland cricket team ?"}
 
 
-quiz_option = { "A) june 22,2021", "b) March 23,2022", "C) june23,2021", 
+quiz_option = {
+"A) june 22,2021", "b) March 23,2022", "C) june23,2021", 
 "A) Trent boult" , "B) Virat kholi", "C) Tim Southee",
- "A) 50" , "B) 40" , "C) 60" ,
- "A) kane Williamson", "B) Rohit poudel", "C) Stephen Fleming"}
+"A) 50" , "B) 40" , "C) 60" ,
+"A) kane Williamson", "B) Rohit poudel", "C) Stephen Fleming"}
 
 quiz_answer = { "A" , "C" , "C" , "C"}
 
-
-
+guess = []
 score = 0
-total = quiz_question(len)
-for items in quiz_question:
- print(items['quiz_question'])
-for quiz_opt in items(['options']):
- print(items['opt'])
-score=+1
+question = 0
+for quiz_question in quiz_questions:
+ print(quiz_question)
+for quiz_option in [question]:
+  print(quiz_option)
 
-if "quiz_question" == "quiz_answer":
-  print('right')
-else:
-  print(f"wrong {'answer'} was the answer" )
-
-percent = (total/score)*100
-
-
-print(f"you got a {'percent'}")
-
-
+quiz_option =+1
