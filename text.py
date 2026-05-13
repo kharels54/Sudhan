3964b43c27ae8700f6a8ac3615ee3ea0c32d4ec2
kharels54 <you@example.com>
2026-05-13 15:16:48 +1200

    new  one 2.0

--- text.py
+++ text.py
@@ -4,27 +4,30 @@ from re import A
 
 print(".....Let's satart the quiz....")
 #crate a list of quiz
-quiz_questions = {
-"When was the New zeland win their frist WTC ? " , 
-"Who's new zeland player have the most wicket in t 20 ? " , 
-"What is the lowest score of New zeland in T20 ? " , 
-"Who is the most wining caption of new zeland cricket team ?"}
+quiz_questions = [
+"When was the New zeland win their frist World Test Campinship ? ", 
+"Who's new zeland player have the most wicket in T20 ? ", 
+"What is the lowest score of New zeland in T20 ? ", 
+"Who is the most wining caption of new zeland cricket team ? "
+]
 
 
-quiz_option = {
-"A) june 22,2021", "b) March 23,2022", "C) june23,2021", 
-"A) Trent boult" , "B) Virat kholi", "C) Tim Southee",
-"A) 50" , "B) 40" , "C) 60" ,
-"A) kane Williamson", "B) Rohit poudel", "C) Stephen Fleming"}
+quiz_options = [
+"A) june 22,2021" "b) March 23,2022" "C) june23,2021", 
+"A) Trent boult" "B) Virat kholi" "C) Tim Southee",
+"A) 50" "B) 40" "C) 60", 
+"A) kane Williamson " "B) Rohit poudel" "C) Stephen Fleming",
+]
 
-quiz_answer = { "A" , "C" , "C" , "C"}
-
-guess = []
+quiz_answer = ["A" , "C" , "C" , "C"]
 score = 0
 question = 0
 for quiz_question in quiz_questions:
- print(quiz_question)
-for quiz_option in [question]:
-  print(quiz_option)
+  answer = input(quiz_question)
+if quiz_options in quiz_questions[question]:
+  print(quiz_options)
+  question += 1
+    
+  
 
-quiz_option =+1
+
