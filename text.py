f33f198d27b510dd2846465fc313b7f9244cf824
kharels54 <you@example.com>
2026-05-19 10:58:51 +1200

    not in progess

--- text.py
+++ text.py
@@ -1,7 +1,6 @@
-guess = []
+
 index = 0
 correct = 0
-question = 0
 print(".....Let's satart the quiz....")
 #crate a list of quiz
 quiz_questions = [
@@ -12,22 +11,29 @@ quiz_questions = [
 ]
 
 
-quiz_options = [
-"A) june 22,2021" "b) March 23,2022" "C) june23,2021\n", 
-"A) Trent boult" "B) Virat kholi" "C) Tim Southee\n",
-"A) 50" "B) 40" "C) 60\n", 
-"A) kane Williamson " "B) Rohit poudel" "C) Stephen Fleming\n,"
+quiz_option = [
+"A) june 22,2021 b) March 23,2022 C) june23,2021\n", 
+"A) Trent boult B) Virat kholi C) Tim Southee\n",
+"A) 50 B) 40 C) 60\n", 
+"A) kane Williamson B) Rohit poudel C) Stephen Fleming\n"
 ]
 
 answer = ["A" , "C" , "C" , "C"]
 
 for quiz_question in quiz_questions:
-  answer = input(quiz_question)
-if quiz_options in quiz_options[index]:
- 
-  print(quiz_options) 
-  index +=1
+ answer = input(quiz_question)
+ for quiz_options in quiz_option:
+  print(quiz_options)
+
+  
   
 
+  index +=1
 
-guess = input('Select[A,B,C,D]')
+  
+
+ 
+
+
+ 
+  
